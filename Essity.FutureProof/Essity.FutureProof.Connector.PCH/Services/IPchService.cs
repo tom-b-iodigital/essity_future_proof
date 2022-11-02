f21e5cef84b6e4593ba9067af44ab56081dcba79
HendrikDanau <hendrik.danau@iodigital.com>
2022-11-02 08:53:47 +0100

    Renamed solution & added PCH connection project

---
+++ Essity.FutureProof/Essity.FutureProof.Connector.PCH/Services/IPchService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Essity.ConsumerTissue.Connector.PCH.Config;
+using Essity.ConsumerTissue.Domain.Interfaces;
+using Essity.ConsumerTissue.Domain.Models;
+
+namespace Essity.ConsumerTissue.Connector.PCH.Services
+{
+    public interface IPchService
+    {
+        Task<CommunicationResult<T>> GetProducts<T>(PchSettingElement setting, string catalog, string lang, string brand)
+            where T : IDomainProduct;
+    }
+}
